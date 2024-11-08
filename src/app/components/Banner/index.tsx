fece7f9340819fb41ba5a14c4d3f7520822c885f
Victor-Lis <109773129+Victor-Lis@users.noreply.github.com>
2024-11-08 01:38:20 -0300

    feat: Adicionando subseção 'Demais Contribuintes' a seção 'Integrantes'

--- src/app/components/Banner/index.tsx
+++ src/app/components/Banner/index.tsx
@@ -14,7 +14,7 @@ export default function Banner() {
     <>
       <div id="banner-up" className="mb-.5/"/>
       <div id="banner" className="mb-2"/>
-      <div className="w-10/12 md:w-5/12 gap-y-5 flex flex-col justify-center items-center" id="banner-box">
+      <div className="w-10/12 md:w-5/12 gap-y-5 flex flex-col justify-center items-center mb-40" id="banner-box">
         <Placar img={TampinhaImg} number={total} description="Tampinhas Coletadas" />
         {/* <Placar img={CadeiraDeRodasImg} number={Math.floor(total/meta) || 0} description="Cadeiras Trocadas"/> */}
         <Description description="Acima são exibidas as tampinhas que foram coletadas pelo nosso protótipo ao longo do tempo, sendo possível inclusive acessar uma análise mais detalhada clicando acima!"/>
--- src/app/components/Integrantes/index.tsx
+++ src/app/components/Integrantes/index.tsx
@@ -7,7 +7,7 @@ export default function Integrantes() {
     <>
       <div className="mb-4" id="integrantes"/>
       <div
-        className="w-10/12 md:w-7/12 gap-y-5 flex flex-col justify-center items-center max-sm:my-10"
+        className="w-10/12 md:w-7/12 flex flex-col justify-center items-center max-sm:my-10"
         id="integrantes-box"
       >
         <Image
@@ -28,6 +28,29 @@ export default function Integrantes() {
             "PEDRO HENRIQUE MORAES SAMSONAS",
             "VICTOR LIS BRONZO",
           ]}
+        />
+        <Card
+          title="Demais Contribuintes"
+          names={[
+            "Beatriz Albertini",
+            "Carlos Augusto",
+            "Driely Cristine",
+            "Karla Samire",
+            "Marília Barbosa",
+            "Paulo Machado",
+            "Rita de Cássia",
+            "Robson Aparecido",
+            "Sérgio Montagner",
+          ]}
+        />
+        <Card
+          title="Descrição dos Demais Contribuintes"
+          names={[
+            "A seção 'Demais Contribuintes', faz questão de dedicar agradecimentos a todos que contribuiram com o projeto em algum momento, mas que não necessariamente se tornaram parte ativa dele a longo prazo.",
+            "Estão inclusas nessa seção pessoas que sugeriram ideias, ajudaram o projeto a enfrentar desafios, incentivaram, fizeram pontes para que o projeto pudesse ir mais longe, etc...",
+            "De maneira geral pessoas que fizeram mesmo que uma pequena, mas importante participação."
+          ]}
+          onlyDescription={true}
         />
       </div>
     </>
