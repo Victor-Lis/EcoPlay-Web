bc38b242560f71df603e80eb878c91f86388dc6a
Victor Lis Bronzo <victorlisbronzo1@gmail.com>
2024-11-08 16:34:32 -0300

    feat: Adicionando colaborador na seção de contribuidores

--- src/app/components/Integrantes/index.tsx
+++ src/app/components/Integrantes/index.tsx
@@ -34,6 +34,7 @@ export default function Integrantes() {
           names={[
             "Beatriz Albertini",
             "Carlos Augusto",
+            "Danilo Pereira",
             "Driely Cristine",
             "Karla Samire",
             "Marília Barbosa",
