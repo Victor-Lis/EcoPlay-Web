f75eb4d1f7bcc18760cb3ece44c0855a4b6e756f
Victor-Lis <109773129+Victor-Lis@users.noreply.github.com>
2024-11-09 00:32:50 -0300

    fix: Concertando erro no monthsContainer, resolvido com formatNum

--- src/app/historico/components/Calendar/DaysContainer/index.tsx
+++ src/app/historico/components/Calendar/DaysContainer/index.tsx
@@ -3,6 +3,7 @@ import type { DayType } from "@/@types/DayType";
 import { useHistoryContext } from "@/@hooks/useHistoryContext";
 import { useEffect, useState } from "react";
 import { getDaysByMonth } from "@/utils/getDaysByMonth"
+import { formatNum } from "@/utils/formatNum";
 
 export default function MonthsContainer({
   dias,
@@ -27,13 +28,14 @@ export default function MonthsContainer({
   return (
     <div className="w-full bg-white border-2 border-green-500 p-2 grid justify-items-center justify-center content-center grid-cols-4 min-[450px]:grid-cols-6 min-[650px]:grid-cols-9 min-[820px]:grid-cols-12 lg:grid-cols-16 xl:grid-cols-18 gap-x-2 gap-y-1">
       {allDays.map((dia) => {
+        selectedMonth?.dias.map((d) => console.log(`${formatNum(Number(d.dia))} === ${formatNum(dia)}`))
         return (
           <button
             type='button'
-            onClick={() => handleSelectDay({ day: selectedMonth?.dias.find((d) => d.dia === `${dia}`) })}
-            onKeyDown={() => handleSelectDay({ day: selectedMonth?.dias.find((d) => d.dia === `${dia}`) })}
-            disabled={!selectedMonth?.dias.find((d) => d.dia === `${dia}`)}
-            className={`py-2 px-4 text-white w-14 text-center border-2 duration-150  ${!selectedMonth?.dias.find((d) => d.dia === `${dia}`) ? "bg-gray-400 cursor-not-allowed": "bg-green-500 hover:border-green-600 hover:opacity-85 hover:scale-[102.5%] cursor-pointer"}`}
+            onClick={() => handleSelectDay({ day: selectedMonth?.dias.find((d) => formatNum(Number(d.dia)) === formatNum(dia)) })}
+            onKeyDown={() => handleSelectDay({ day: selectedMonth?.dias.find((d) => formatNum(Number(d.dia)) === formatNum(dia)) })}
+            disabled={!selectedMonth?.dias.find((d) => formatNum(Number(d.dia)) === formatNum(dia))}
+            className={`py-2 px-4 text-white w-14 text-center border-2 duration-150  ${!selectedMonth?.dias.find((d) => formatNum(Number(d.dia)) === formatNum(dia)) ? "bg-gray-400 cursor-not-allowed": "bg-green-500 hover:border-green-600 hover:opacity-85 hover:scale-[102.5%] cursor-pointer"}`}
             key={`${dia}`}
           >
             {dia}
